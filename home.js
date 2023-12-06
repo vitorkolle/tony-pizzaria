be63b8b6ab16cf1014cc992926cc2a7c19b66355
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2023-12-06 17:18:21 -0300

    começo da implementação da api

---
+++ home.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const urlCategorias = 'http://localhost:8080/categorias' 
+const urlProdutos = 'http://localhost:8080/produtos'
+const urlProduto = `http://localhost:8080/produto/?produto=${produto}`
+const urlCliente = `http://localhost:8080/cliente/?nome=${nome}`
+const urlComentarios = 'http://localhost:8080/comentarios'
+
+
+
