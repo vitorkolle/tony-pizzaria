6ac3e6d0af63b153a2aa596272ad8e6c369a9adf
vitorkolle <vitorpaeskolle@gmail.com>
2023-12-11 11:22:40 -0300

    utilização da api em andamento

--- home.js
+++ home.js
@@ -2,9 +2,21 @@
 
 const urlCategorias = 'http://localhost:8080/categorias' 
 const urlProdutos = 'http://localhost:8080/produtos'
-const urlProduto = `http://localhost:8080/produto/?produto=${produto}`
-const urlCliente = `http://localhost:8080/cliente/?nome=${nome}`
-const urlComentarios = 'http://localhost:8080/comentarios'
+// const urlProduto = `http://localhost:8080/produto/?produto=${produto}`
+// const urlCliente = `http://localhost:8080/cliente/?nome=${nome}`
+// const urlComentarios = 'http://localhost:8080/comentarios' 
+
+async function criarCategorias(){
+    const responseCat = await fetch(urlCategorias)
+    const dadosCat = await responseCat.json()
+    console.log(dadosCat)
+}
+
+
+
+//area de teste
+criarCategorias()
+
 
 
 
