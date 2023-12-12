91a7f38e5e773b6f267c0753dce60434d364e642
vitorkolle <vitorpaeskolle@gmail.com>
2023-12-12 17:56:53 -0300

    resolução dos bugs ao requisitar a api

--- home.js
+++ home.js
@@ -6,16 +6,35 @@ const urlProdutos = 'http://localhost:8080/produtos'
 // const urlCliente = `http://localhost:8080/cliente/?nome=${nome}`
 // const urlComentarios = 'http://localhost:8080/comentarios' 
 
-async function criarCategorias(){
+async function pegarCategorias(){
     const responseCat = await fetch(urlCategorias)
     const dadosCat = await responseCat.json()
-    console.log(dadosCat)
+    const arrayCategorias = dadosCat.categorias
+    const imgLocal = []
+
+    arrayCategorias.forEach(categorias => {
+        imgLocal.push(categorias.img_categoria)        
+    });
+
+    return imgLocal
+}
+
+function criarCategorias(
+    
+){
+    console.log(imgLocal)
+
+    imgLocal.forEach(categorias => {
+                
+    });
+
 }
 
 
 
 //area de teste
-criarCategorias()
+  //pegarCategorias()
+  criarCategorias()
 
 
 
