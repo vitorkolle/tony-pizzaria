beed3dc25a7a96dcb08333cfba067daa92a6d209
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2023-12-13 15:40:18 -0300

    primeira requisição realizada

--- home.js
+++ home.js
@@ -5,37 +5,36 @@ const urlProdutos = 'http://localhost:8080/produtos'
 // const urlProduto = `http://localhost:8080/produto/?produto=${produto}`
 // const urlCliente = `http://localhost:8080/cliente/?nome=${nome}`
 // const urlComentarios = 'http://localhost:8080/comentarios' 
+var imgCatLocal = []
 
 async function pegarCategorias(){
     const responseCat = await fetch(urlCategorias)
     const dadosCat = await responseCat.json()
     const arrayCategorias = dadosCat.categorias
-    const imgLocal = []
 
     arrayCategorias.forEach(categorias => {
-        imgLocal.push(categorias.img_categoria)        
+        imgCatLocal.push(categorias.img_categoria)        
     });
 
-    return imgLocal
+    return imgCatLocal
 }
 
-function criarCategorias(
-    
-){
-    console.log(imgLocal)
+function criarCategorias(imgCatLocal){
+        const container_categorias = document.getElementById('container-categorias')
+        const imagens_categorias = document.createElement('img')
+        imagens_categorias.src = imgCatLocal
+        container_categorias.appendChild(imagens_categorias)    
+    }
 
-    imgLocal.forEach(categorias => {
-                
-    });
 
+async function mostrarCategorias(){
+    const imagens_categorias_final = await pegarCategorias()
+    imagens_categorias_final.forEach(criarCategorias)
 }
-
 
 
 //area de teste
-  //pegarCategorias()
-  criarCategorias()
-
+mostrarCategorias()
 
 
 
